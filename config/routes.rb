b254ea36a32069f747288a9d8b0b7ee882a2a459
Pofigist <optimizmforever@gmail.com>
2014-01-09 11:28:11 +0300

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Spree::Core::Engine.routes.draw do
     resource :yandex_market_settings, only: [:edit, :update]
     get "/yandex_market_settings/taxonomy/:id" => "yandex_market_settings#taxonomy"
     post "/yandex_market_settings/export" => "yandex_market_settings#export"
+    get "/yandex_market_settings/taxonomies/search" => "yandex_market_settings#taxonomies_search"
+    get "/yandex_market_settings/taxonomies/taxons" => "yandex_market_settings#taxonomy_taxons"
   end
 end
