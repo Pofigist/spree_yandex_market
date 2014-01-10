e857974a4e9b7a2007a13c4f1a258bde66d3b4d2
Pofigist <optimizmforever@gmail.com>
2014-01-10 12:14:58 +0300

    route for find properties

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Spree::Core::Engine.routes.draw do
     post "/yandex_market_settings/export" => "yandex_market_settings#export"
     get "/yandex_market_settings/taxonomies/search" => "yandex_market_settings#taxonomies_search"
     get "/yandex_market_settings/taxonomies/taxons" => "yandex_market_settings#taxonomy_taxons"
+    get "/yandex_market_settings/properties/find" => "yandex_market_settings#properties_find"
   end
 end
