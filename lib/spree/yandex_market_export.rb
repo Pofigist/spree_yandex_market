d67c0c55376092322e6acf9f2d26cf3350fc4a66
Pofigist <optimizmforever@gmail.com>
2014-01-13 10:00:40 +0300

    It only for testing memory

--- lib/spree/yandex_market_export.rb
+++ lib/spree/yandex_market_export.rb
@@ -4,70 +4,71 @@ require 'nokogiri'
 class YandexMarketExport
 
     def initialize
-        @builder = Nokogiri::XML::Builder.new encoding: 'UTF-8' do |xml|
-            xml.doc.create_internal_subset('yml_catalog', nil, 'shops.dtd')
-            xml.yml_catalog(date: Time.now.strftime("%Y-%m-%d %H:%M")) {
-                shop xml
-            }
-        end
+        @file =  File.new(Rails.root.join('public', y(:file_path)), 'w:UTF-8')
+        @file.truncate(0)
+        @file.puts("<yml_catalog date=\"#{Time.now.strftime("%Y-%m-%d %H:%M")}\">")
+        shop
+        @file.puts("</yml_catalog>")
+        @file.close
     end
 
-    def shop(xml)
-        xml.shop do |s|
-            s.name        y(:shop_name)
-            s.company     y(:shop_company)
-            s.url         Spree::Config.site_url
-            s.platform    "Spree"
-            s.version     Spree.version
-            s.agency      y(:shop_agency)
-            s.email       y(:shop_email)
-
-            currencies s
-            categories s
-            offers s
-        end
+    def shop
+        @file.puts("<name>#{y(:shop_name)}</name>")
+        @file.puts("<company>#{ y(:shop_company) }</company>")
+        @file.puts("<url>#{Spree::Config.site_url}</url>")
+        @file.puts("<platform>#{ "Spree" }</platform>")
+        @file.puts("<version>#{ Spree.version}</version>")
+        @file.puts("<agency>#{y(:shop_agency)}</agency>")
+        @file.puts("<email>#{y(:shop_email)}</email>")
+        currencies
+        categories
+        offers
+        return 1
     end
 
-    def currencies(xml)
-        xml.currencies {
-            xml.currency id: Spree::Config[:currency], rate: 1
-        }
+    def currencies
+        @file.puts("<currencies>")
+        @file.puts("<currency id=\"#{Spree::Config[:currency]}\" rate=\"#{1}\"/>")
+        @file.puts("</currencies>")
+        return 1
     end
 
-    def categories(xml)
-        xml.categories {
-            Spree::Taxonomy.where("spree_taxonomies.id in (#{y(:cat_taxonomy_ids)})").each do |e|
-                cats(e).each do |c|
-                    params = {id: c.id}
-                    params[:parentId] = c.parent_id if c.parent_id
-                    xml.category c.name, params
-                end
+    def categories
+        @file.puts("<categories>")
+        Spree::Taxonomy.where("spree_taxonomies.id in (#{y(:cat_taxonomy_ids)})").each do |e|
+            cats(e).each do |c|
+                @file.puts("<category id=\"#{c.id}\" parentId=\"#{c.parent_id}\">#{c.name}</category>")
             end
-        }
+        end
+        @file.puts("</categories>")
+        return 1
     end
 
-    def offers(xml)
-        xml.offers {
-            products.each do |p|
-                xml.offer id: p.id, type: "vendor.model", available: true do |o|
-                    category = p.taxons.where("spree_taxons.taxonomy_id in (#{y(:cat_taxonomy_ids)})").first
-                    o.url         "#{Spree::Config.site_url}/products/#{p.permalink}"
-                    o.price       p.price.to_i
-                    o.currencyId  Spree::Config[:currency]
-                    o.categoryId  category.id if category
-                    p.images.first(10).each { |i| o.picture "#{Spree::Config.site_url}#{i.attachment.url(:original)}" }
-                    o.store       y(:store)
-                    o.pickup      y(:pickup)
-                    o.delivery    y(:delivery)
-                    o.typePrefix  p.name
-                    o.vendor      p.property(vendor_prop.name) if vendor_prop
-                    o.model_      p.property(model_prop.name) if model_prop
-                    o.description p.description
-                    o.adult       y(:adult) if y(:adult)
-                    o.age         y(:age) unless y(:age) == 0
-                end
+    def offers
+        @file.puts("<offers>")
+        products.each do |p|
+            @file.puts("<offer id=\"#{p.id}\" type=\"vendor.model\" available=\"true\">")
+            category = p.taxons.where("spree_taxons.taxonomy_id in (#{y(:cat_taxonomy_ids)})").first
+            @file.puts("<url>#{Spree::Config.site_url}/products/#{p.permalink}</url>")
+            @file.puts("<price>#{p.price}</price>")
+            @file.puts("<currencyId>#{Spree::Config[:currency]}</currencyId>")
+            @file.puts("<categoryId>#{category.id}</categoryId>") if category.id
+            p.images.first(10).each do |i|
+                @file.puts("<picture>#{Spree::Config.site_url}#{i.attachment.url(:original)}</picture>")
             end
-        }
+            @file.puts("<store>#{y(:store)}</store>")
+            @file.puts("<pickup>#{y(:pickup)}</pickup>")
+            @file.puts("<delivery>#{y(:delivery)}</delivery>")
+            @file.puts("<typePrefix>#{p.name}</typePrefix>")
+            @file.puts("<vendor>#{p.property(vendor_prop.name)}</vendor>") if vendor_prop
+            @file.puts("<model_>#{p.property(model_prop.name)}</model_>") if model_prop
+            @file.puts("<description>#{p.description}</description>")
+            @file.puts("<adult>#{y(:adult)}</adult>") if y(:adult)
+            @file.puts("<age>#{y(:age)}</age>") if y(:age) != 0
+            @file.puts("</offer>")
+        end
+        @file.puts("</offers>")
+        return 1
     end
 
     def cats(taxonomy)
@@ -85,14 +86,6 @@ class YandexMarketExport
 
     def vendor_prop
         Spree::Property.find(y :vendor_prop)
-    end
-
-    def properties
-        Spree::Property.all
-    end
-
-    def out
-        @builder.to_xml
     end
 
     private
